1f518bd0b9154f59505a038047d3e3da46970b5e
emanuelpps <emanuelpages.ps@gmail.com>
2025-03-26 21:30:33 -0300

    fix: added height correction to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 const NavBar: React.FC = () => {
   const [hashSection, setHashSection] = useState<string>("");
   return (
-    <nav className="w-[100%] flex justify-center items-center h-[8vh]">
+    <nav className="w-[100%] flex justify-center items-center h-[9vh]">
       <div className="flex w-[80%] bg-black items-center text-white justify-between h-[100%] rounded-4xl p-2">
         <NavLinksLeft
           hashSection={hashSection}
