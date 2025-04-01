890f0a0aeb2bb596a8f262cbe282115dcb3d02b5
emanuelpps <emanuelpages.ps@gmail.com>
2025-03-31 22:55:05 -0300

    fix: fix in tabButton refactor

--- src/components/Buttons/TabButton.tsx
+++ src/components/Buttons/TabButton.tsx
@@ -1,39 +1,19 @@
 import { Button } from "./Types";
-import { JSX } from "react";
-
 export class TabButton implements Button {
   label: string;
-  activeLabel?: string;
-  setTitleSelection?: (value: string) => void;
 
-  constructor(
-    label: string,
-    activeLabel?: string,
-    setTitleSelection?: (value: string) => void
-  ) {
+  constructor(label: string) {
     this.label = label;
-    this.activeLabel = activeLabel;
-    this.setTitleSelection = setTitleSelection;
   }
 
-  render = (): JSX.Element => {
-
-    const handleChange = (value:string) => {
-      this.setTitleSelection(value)
-    }
+  render() {
     return (
       <button
-        className={`px-6 py-3 rounded-t-3xl transition font-semibold cursor-pointer ${
-          this.activeLabel === this.label
-            ? "bg-[#FF6900] text-white hover:bg-[#FCB173]"
-            : "bg-white text-black hover:bg-gray-200"
-        }`}
-        onClick={() => handleChange(this.label)}
+        className="px-6 py-3 bg-white text-black rounded-t-3xl hover:bg-gray-200 transition font-semibold cursor-pointer"
+        onClick={() => "do something"}
       >
         {this.label}
       </button>
     );
-  };
+  }
 }
-
-export default TabButton;
