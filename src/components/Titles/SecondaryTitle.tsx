7d30318b79a62e5655185ac3869295a1592a57a5
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-03 19:22:47 -0300

    feat: added style to subtitle to skill section

--- src/components/Titles/SecondaryTitle.tsx
+++ src/components/Titles/SecondaryTitle.tsx
@@ -11,7 +11,7 @@ export class SecondaryTitle implements TitlesProp {
   render() {
     return (
       <div className="flex flex-col justify-start items-start">
-        <h2 className="font-semibold text-4xl text-white">{this.subtitle}</h2>
+        <h2 className="font-light text-4xl text-white">{this.subtitle}</h2>
         <h1 className="font-semibold text-5xl text-[#FF4D7D]">{this.title}</h1>
       </div>
     );
