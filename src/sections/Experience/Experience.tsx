829a2eb1c9dd77eea399bfdf82fea283e44d365f
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-04 18:59:47 -0300

    feat: added id and scroll redirect

--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -2,7 +2,10 @@ import ExperienceContainer from "./components/ExperienceContainer";
 
 const Experience = () => {
   return (
-    <section className="w-full min-h-screen flex justify-center items-center text-white flex-col">
+    <section
+      id="experience"
+      className="w-full min-h-screen flex justify-center items-center text-white flex-col"
+    >
       <ExperienceContainer />
     </section>
   );
--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -2,7 +2,7 @@ import { SkillsContainer } from "./components/SkillsContainer";
 
 const Skills = () => {
   return (
-    <section className="w-full min-h-screen flex justify-center items-center bg-[#0F1621] text-white">
+    <section id="skills" className="w-full min-h-screen flex justify-center items-center bg-[#0F1621] text-white">
       <SkillsContainer />
     </section>
   );
