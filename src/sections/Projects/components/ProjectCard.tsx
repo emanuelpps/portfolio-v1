9e13a449fa04f2b9c55920c910b43bd9ed05dae8
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-09 00:10:07 -0300

    feat: added styles corrections to project cards

--- src/sections/Projects/components/ProjectCard.tsx
+++ src/sections/Projects/components/ProjectCard.tsx
@@ -29,7 +29,7 @@ const ProjectCard: FC<ProjectCardProps> = ({
       </div>
       <motion.div
         className="relative w-85 h-[420px] overflow-hidden rounded-[40px]"
-        whileHover={{ scale: 1.05 }}
+        whileHover={{ scale: 1.02 }}
         transition={{ type: "spring", stiffness: 200 }}
       >
         <div className="absolute bottom-0 w-[75%] h-[325px] bg-[#31343a79] rounded-t-[40px] left-1/2 -translate-x-1/2 z-10" />
@@ -37,12 +37,14 @@ const ProjectCard: FC<ProjectCardProps> = ({
         <img
           src={image}
           alt={title}
-          className="absolute bottom-0 left-0 w-full h-[300px] object-cover rounded-[40px] z-30"
+          className="absolute bottom-0 left-0 w-full h-[300px] object-cover rounded-[40px] z-[30]"
         />
+        <div className="absolute right-[0px] top-[298px] w-[40px] h-[40px] z-[95] bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
+        <div className="absolute left-[166px] bottom-[1px] w-[40px] h-[40px] z-[95] bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
       </motion.div>
       <div
         id="tab-curve"
-        className="absolute overflow-visible bottom-0 right-0 z-50 flex items-center justify-center after:bg-[#0F1724] rounded-l-[50px] p-[1rem] after:w-[240px] after:h-[130px] after:content-[''] after:absolute after:top-[-5] after:rounded-tl-[40px] after:rounded-r-none"
+        className="absolute overflow-visible bottom-0 right-0 z-50 flex items-center justify-center after:bg-[#0F1724] rounded-l-[50px] p-[1rem] after:w-[240px] after:h-[127px] after:content-[''] after:absolute after:top-[-5] after:rounded-tl-[40px] after:rounded-r-none"
       >
         <p>
           <span></span>
