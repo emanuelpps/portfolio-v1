97182bdb1fd28b209a9770e1fe2dc15780a760e6
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-22 21:42:24 -0300

    feat: added header and separators to footer section

--- src/sections/Footer/components/Header.tsx
+++ src/sections/Footer/components/Header.tsx
@@ -1,5 +1,17 @@
+import { ButtonFactory } from "../../../components/Buttons/ButtonFactory";
+import { TitlesFactory } from "../../../components/Titles/TitlesFactory";
 const Header = () => {
-  return <div>Header</div>;
+  const FooterTitle = TitlesFactory.createTitle("secondary", "Lets Connect");
+  const ConnectButton = ButtonFactory.createButton({
+    type: "primary",
+    label: "Connect with me",
+  });
+  return (
+    <div className="flex justify-between w-full">
+      {FooterTitle.render()}
+      {ConnectButton.render()}
+    </div>
+  );
 };
 
 export default Header;
--- src/sections/Footer/components/Separator.tsx
+++ src/sections/Footer/components/Separator.tsx
@@ -1,5 +1,5 @@
 const Separator = () => {
-  return <div>Separator</div>;
+  return <div className="w-full h-px my-4 bg-gray-700 opacity-40" />;
 };
 
 export default Separator;
