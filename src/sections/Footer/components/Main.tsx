fc9d6c7df743c0415e2466a66fa9eb206a97bda7
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-22 21:51:06 -0300

    feat: added hardcode strings to footer

--- src/sections/Footer/components/Main.tsx
+++ src/sections/Footer/components/Main.tsx
@@ -1,5 +1,32 @@
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="flex">
+      <div className="flex flex-col justify-center w-full">
+        <div>Logo</div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          modi, ratione dolores nesciunt corporis sit ex aliquam. Pariatur
+          reprehenderit maiores velit odit magni quos, nulla excepturi doloribus
+          aperiam voluptate? Dolores.
+        </div>
+        <div>SOCIAL MEDIA</div>
+      </div>
+      <div className="flex justify-center w-full gap-10">
+        <div className="flex flex-col">
+          <div>Navigation</div>
+          <div>Navigation</div>
+          <div>Navigation</div>
+          <div>Navigation</div>
+        </div>
+        <div className="flex flex-col">
+          <div>Navigation</div>
+          <div>Navigation</div>
+          <div>Navigation</div>
+          <div>Navigation</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
