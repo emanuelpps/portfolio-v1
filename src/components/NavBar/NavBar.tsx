149ac90d371fdc5f1317cc01b483cbeb595e52df
Emanuel Pagés <101524982+emanuelpps@users.noreply.github.com>
2025-06-13 19:37:06 -0300

    chore: added corrections to mobile navbar (#46)

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "react";
 import LogoContainer from "./components/LogoContainer";
 import NavLinksLeft from "./components/NavLinksLeft";
 import NavLinksRight from "./components/NavLinksRight";
-import { useState } from "react";
 
 const NavBar: React.FC = () => {
   const [hashSection, setHashSection] = useState<string>("");
-  return (
+  const [screenWidth, setScreenWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const updateWidth = () => setScreenWidth(window.innerWidth);
+
+    updateWidth();
+
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
+  return screenWidth <= 1023 ? (
+    <nav></nav>
+  ) : (
     <nav className="fixed top-5 left-0 w-full flex justify-center items-center h-[10vh] z-50">
       <div className="flex w-[85%] bg-gray-900/50 backdrop-blur-lg shadow-lg items-center justify-between h-full rounded-2xl px-6 py-3 text-white border border-gray-800">
         <NavLinksLeft
