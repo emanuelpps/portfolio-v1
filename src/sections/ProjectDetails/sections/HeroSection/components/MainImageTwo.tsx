db10da4ef8088b27a11f9850676dd2e44d79ef15
emanuelpps <emanuelpages.ps@gmail.com>
2025-06-13 17:05:20 -0300

    chore: main image dimension correction

--- src/sections/ProjectDetails/sections/HeroSection/components/MainImageTwo.tsx
+++ src/sections/ProjectDetails/sections/HeroSection/components/MainImageTwo.tsx
@@ -7,20 +7,20 @@ interface MainImageTwoProps {
 
 export const MainImageTwo = ({ image, type }: MainImageTwoProps) => {
   return (
-    <div className="relative flex items-center justify-center w-[60%] mt-2 rounded-2xl overflow-hidden bg-[#0F1724] ">
+    <div className="relative flex items-center justify-center w-full mt-2 rounded-2xl overflow-hidden bg-[#0F1724] ">
       <img
         src={image}
         className={
           type === TypeOfProject.PROJECT
-            ? "w-[80%] rounded-2xl"
-            : "w-[50%] object-contain rounded-2xl "
+            ? "w-[90%] rounded-2xl"
+            : "w-[50%] object-contain rounded-2xl"
         }
         alt="Main visual"
       />
       <div
         className={`pointer-events-none ${
           type === TypeOfProject.PROJECT
-            ? "absolute bottom-0 w-[80%] h-40 bg-gradient-to-b from-transparent to-[#0F1724]"
+            ? "absolute bottom-0 w-[90%] h-40 bg-gradient-to-b from-transparent to-[#0F1724]"
             : ""
         }`}
       />
