d9717ce587c9de29656ccf3495b2907f8be909d1
emanuelpps <emanuelpages.ps@gmail.com>
2025-06-17 18:26:34 -0300

    chore: bg color correction

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -17,7 +17,7 @@ const NavBar: React.FC = () => {
   }, []);
 
   return screenWidth <= 1023 ? (
-    <nav></nav>
+    <nav className="w-full bg-transparent h-[100vh] border-none"></nav>
   ) : (
     <nav className="fixed top-5 left-0 w-full flex justify-center items-center h-[10vh] z-50">
       <div className="flex w-[85%] bg-gray-900/50 backdrop-blur-lg shadow-lg items-center justify-between h-full rounded-2xl px-6 py-3 text-white border border-gray-800">
