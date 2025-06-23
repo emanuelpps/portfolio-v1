6081919ff61d675e733db0f303a0bf068ee1dcce
emanuelpps <emanuelpages.ps@gmail.com>
2025-06-23 20:24:56 -0300

    chore: added title layout corrections

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -2,7 +2,7 @@ import { SkillsContainer } from "./components/SkillsContainer";
 
 const Skills = () => {
   return (
-    <section id="skills" className="w-full min-h-screen flex justify-center items-center bg-[#0F1621] text-white">
+    <section id="skills" className="w-[50%] md:w-full min-h-screen flex justify-center items-center bg-[#0F1621] text-white">
       <SkillsContainer />
     </section>
   );
