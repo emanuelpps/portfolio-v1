8a7400cde6650354223e6c74690929551ae6cca8
Emanuel Pagés <101524982+emanuelpps@users.noreply.github.com>
2025-06-25 17:12:36 -0300

    Feat/responsive (#55)
    * chore: added corrections to mobile navbar
    
    * chore: bg color correction
    
    * chore: added mobile menu icon and dropdown layout
    
    * chore: added logo animation to resonsive navbar
    
    * chore: fix in navbar mobile links
    
    * chore: added padding fix in mobile menu
    
    * chore: added correction to hero section
    
    * chore: added title layout corrections
    
    * chore: added hero section correction
    
    * chore: added skills selection buttons
    
    * chore: added dropdown to skills seleector

--- src/components/Buttons/SelectionToggle.tsx
+++ src/components/Buttons/SelectionToggle.tsx
@@ -14,19 +14,20 @@ export class SelectionToggle implements Button {
     this.isActive = isActive;
     this.setActiveTab = setActiveTab;
   }
+
   render() {
     return (
       <button
-        className={`px-6 py-3 w-[100px] rounded-xl font-semibold transition-all cursor-pointer duration-150 ease-in-out
-        ${
-          this.isActive
-            ? "bg-[#FF4D7D] text-white shadow-inner translate-y-[1px] scale-[0.98]"
-            : "bg-gray-800 text-gray-300 hover:bg-[#FF4D7D] hover:text-white hover:shadow-lg shadow-md active:scale-[0.97] active:translate-y-[1px]"
-        }
-      `}
+        className={`flex items-center justify-between px-4 py-3 w-[160px] rounded-xl font-semibold text-sm transition-all cursor-pointer duration-150 ease-in-out
+          ${
+            this.isActive
+              ? "bg-[#FF4D7D] text-white shadow-inner translate-y-[1px] scale-[0.98]"
+              : "bg-gray-800 text-gray-300 hover:bg-[#FF4D7D] hover:text-white hover:shadow-lg shadow-md active:scale-[0.97] active:translate-y-[1px]"
+          }
+        `}
         onClick={() => this.setActiveTab(this.label)}
       >
-        {this.label}
+        <span>{this.label}</span>
       </button>
     );
   }
