9c554dfa6da4df180ee9e67cbcd13cbf22e02c2c
emanuelpps <emanuelpages.ps@gmail.com>
2025-06-25 16:56:59 -0300

    chore: added dropdown to skills seleector

--- src/sections/Skills/components/SkillsTabs.tsx
+++ src/sections/Skills/components/SkillsTabs.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { ButtonFactory } from "../../../components/Buttons/ButtonFactory";
+import { FiChevronDown } from "react-icons/fi";
 
 interface SkillsProps {
   activeTab: string;
@@ -9,6 +10,7 @@ interface SkillsProps {
 const tabs = ["Frontend", "Native", "Testing", "Backend", "Tools"];
 
 const SkillsTabs = ({ activeTab, setActiveTab }: SkillsProps) => {
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
   const [screenWidth, setScreenWidth] = useState<number>(0);
 
   useEffect(() => {
@@ -21,19 +23,28 @@ const SkillsTabs = ({ activeTab, setActiveTab }: SkillsProps) => {
   const isMobile = screenWidth <= 1023;
 
   return isMobile ? (
-    <div className="flex justify-center w-[90vw] gap-4 pt-20 flex-wrap-reverse">
-      {tabs.map((tab) => {
-        const button = ButtonFactory.createButton({
-          type: "selectionToggler",
-          label: tab,
-          isActive: tab === activeTab,
-          setActiveTab,
-        });
-        return <div key={tab}>{button.render()}</div>;
-      })}
-    </div>
+    <section className="flex flex-col items-center justify-center w-[90vw] gap-4 pt-20 flex-wrap">
+      <div
+        className="relative flex flex-col justify-between p-3.5 rounded-4xl items-center w-full bg-[#FF4D7D] text-white shadow-inner translate-y-[1px] scale-[0.98] z-[50]"
+        onClick={() => setShowDropDown(!showDropDown)}
+      >
+        <div className="flex justify-between w-full">
+          {activeTab}
+          <FiChevronDown size={25} className="text-white" />
+        </div>
+        {showDropDown && (
+          <ul className="absolute p-3.5 rounded-b-4xl w-full bg-[#FF4D7D] text-white shadow-inner translate-y-[1px] scale-[0.98] top-10">
+            {tabs.map((tab) => (
+              <li className="flex pt-5" onClick={() => setActiveTab(tab)}>
+                {tab}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </section>
   ) : (
-    <div className="flex justify-center md:justify-end w-[80vw] gap-5 pt-10">
+    <section className="flex justify-center md:justify-end w-[80vw] gap-5 pt-10">
       {tabs.map((tab) => {
         const button = ButtonFactory.createButton({
           type: "tab",
@@ -41,10 +52,9 @@ const SkillsTabs = ({ activeTab, setActiveTab }: SkillsProps) => {
           isActive: tab === activeTab,
           setActiveTab,
         });
-
         return <div key={tab}>{button.render()}</div>;
       })}
-    </div>
+    </section>
   );
 };
 
