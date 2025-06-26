5fced87847a22ce941f6cdabc068f36cd5b3c857
Emanuel Pagés <101524982+emanuelpps@users.noreply.github.com>
2025-06-26 18:05:14 -0300

    Feat/responsive (#59)
    * chore: added corrections to mobile navbar
    
    * chore: bg color correction
    
    * chore: added mobile menu icon and dropdown layout
    
    * chore: added logo animation to resonsive navbar
    
    * chore: fix in navbar mobile links
    
    * chore: added padding fix in mobile menu
    
    * chore: added correction to hero section
    
    * chore: added title layout corrections
    
    * chore: added hero section correction
    
    * chore: added skills selection buttons
    
    * chore: added dropdown to skills seleector
    
    * chore: added skills icon correction
    
    * chore: correction in skill title, now is a dropdown selector
    
    * chore: added corrections to skills container
    
    * chore: added width correction to skill container
    
    * chore: added experience

--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -4,7 +4,7 @@ const Experience = () => {
   return (
     <section
       id="experience"
-      className="w-full min-h-screen flex justify-center items-center text-white flex-col"
+      className="max-w-[100vw] md:w-full min-h-screen flex justify-center items-center text-white flex-col"
     >
       <ExperienceContainer />
     </section>
--- src/sections/Experience/components/ExperienceItem.tsx
+++ src/sections/Experience/components/ExperienceItem.tsx
@@ -23,21 +23,21 @@ const ExperienceItem: FC<ExperienceTypeProps> = ({
 }) => {
   return (
     <motion.div
-      className="relative flex text-white md:items-center md:justify-between"
+      className="relative flex flex-row text-white md:items-center md:justify-between"
       initial={{ opacity: 0, x: -50 }}
       whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
       viewport={{ once: true }}
     >
       <div className="w-full text-left">
-        <h3 className="text-xl font-semibold">{CompanyName}</h3>
-        <p className="text-lg text-gray-400">{Period}</p>
+        <h3 className="text-lg font-semibold md:text-xl">{CompanyName}</h3>
+        <p className="text-gray-400 text-md">{Period}</p>
       </div>
-      <div className="relative z-10 justify-center hidden w-full md:flex">
+      <div className="relative z-10 flex justify-center w-[50%] md:w-full">
         <CircleSeparator itemType={ItemType} />
       </div>
-      <div className="w-full text-left">
-        <h3 className="text-xl font-semibold">{JobTitle}</h3>
+      <div className="w-full text-right md:text-left">
+        <h3 className="text-lg font-semibold md:text-xl">{JobTitle}</h3>
       </div>
     </motion.div>
   );
