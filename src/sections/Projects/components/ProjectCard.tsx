82cc41521341d250a32737064b23cae8ebed5530
Emanuel Pagés <101524982+emanuelpps@users.noreply.github.com>
2025-06-30 17:37:31 -0300

    Feat/responsive (#62)
    * chore: added corrections to mobile navbar
    
    * chore: bg color correction
    
    * chore: added mobile menu icon and dropdown layout
    
    * chore: added logo animation to resonsive navbar
    
    * chore: fix in navbar mobile links
    
    * chore: added padding fix in mobile menu
    
    * chore: added correction to hero section
    
    * chore: added title layout corrections
    
    * chore: added hero section correction
    
    * chore: added skills selection buttons
    
    * chore: added dropdown to skills seleector
    
    * chore: added skills icon correction
    
    * chore: correction in skill title, now is a dropdown selector
    
    * chore: added corrections to skills container
    
    * chore: added width correction to skill container
    
    * chore: added experience
    
    * chore: refactor projectdetails
    
    * chore: correction project card for mobile version
    
    * chore: corner fix in project card

--- src/sections/Projects/components/ProjectCard.tsx
+++ src/sections/Projects/components/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
         <div className="absolute top-4 pl-4 z-20 border-b-[1px] w-full border-white pb-3">
           <h3 className="text-lg font-semibold text-white">{project.title}</h3>
           <div className="flex gap-2 text-white text-[0.7rem] font-extralight">
-            {project.stack.map((lang) => (
+            {project.stack.map((lang: string) => (
               <p>{lang}</p>
             ))}
           </div>
@@ -31,8 +31,8 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
             alt={project.title}
             className="absolute bottom-0 left-0 md:w-full md:h-[300px] object-cover rounded-[40px] z-[30]"
           />
-          <div className="absolute right-[0px] top-[298px] md:w-[40px] h-[40px] z-[95] bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
-          <div className="absolute left-[166px] bottom-[1px] md:w-[40px] h-[40px] z-[95] bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
+          <div className="absolute right-[0px] top-[298px] w-[20px] md:w-[40px] h-[40px] z-[95] bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
+          <div className="absolute left-[165.5px] md:left-[166px] bottom-[1px] md:w-[40px] w-[20px] h-[40px] z-[95] bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
         </motion.div>
         <div
           id="tab-curve"
