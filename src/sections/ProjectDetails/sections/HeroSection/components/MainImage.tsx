2c2b66c783d3931e87448d6edd743d5b945e33f4
emanuelpps <emanuelpages.ps@gmail.com>
2025-07-08 18:02:47 -0300

    chore: fix in main image project details

--- src/sections/ProjectDetails/sections/HeroSection/components/MainImage.tsx
+++ src/sections/ProjectDetails/sections/HeroSection/components/MainImage.tsx
@@ -4,8 +4,8 @@ interface MainImageProps {
 export const MainImage = ({ image }: MainImageProps) => {
   return (
     <div className="relative flex items-center justify-center md:w-full w-[95vw]">
-      <div className="absolute top-0 md:w-[80%] h-40 bg-gradient-to-t from-transparent to-[#0F1724] pointer-events-none" />
-      <img src={image} className="md:w-[80%] rounded-2xl md:h-full h-[40vh]" />
+      <div className="absolute top-0 md:w-[80%] w-full md:h-40 h-20 bg-gradient-to-t from-transparent to-[#0F1724] pointer-events-none" />
+      <img src={image} className="md:w-[80%] rounded-2xl md:h-full h-full" />
     </div>
   );
 };
