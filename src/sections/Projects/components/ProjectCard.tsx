f968516c5f03f040cf7a1ca542d9480d2b4debb8
emanuelpps <emanuelpages.ps@gmail.com>
2025-07-25 23:03:01 -0300

    fix: curves card project details

--- src/sections/Projects/components/ProjectCard.tsx
+++ src/sections/Projects/components/ProjectCard.tsx
@@ -9,39 +9,42 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   return (
-    <div className="relative w-[90vw] lg:w-85 h-full lg:h-[420px] rounded-[40px] border-[1px] border-r-transparent border-t-white/10 border-b-transparent border-l-white/10 bg-gradient-to-b from-white/5 to-transparent backdrop-blur-md shadow-xl hover:bg-[#FF4D7D] transition-colors duration-500 cursor-pointer">
-      <Link to={`/project/${project.id}`} state={project}>
-        <div className="absolute top-4 pl-4 z-20 border-b-[1px] w-full border-white pb-3">
-          <h3 className="text-lg font-semibold text-white">{project.title}</h3>
-          <div className="flex gap-2 text-white text-[0.7rem] font-extralight">
-            {project.stack.map((lang: string) => (
-              <p>{lang}</p>
-            ))}
-          </div>
+    <div className="relative w-[90vw] lg:w-85 h-full lg:h-[420px] rounded-[40px] border-[1px] border-r-transparent border-t-white/10 border-b-transparent border-l-white/10 bg-gradient-to-b from-white/5 to-transparent backdrop-blur-md shadow-xl hover:bg-[#FF4D7D] transition-colors duration-500 cursor-pointer overflow-visible">
+      {/* HEADER */}
+      <div className="absolute top-4 pl-4 z-30 border-b-[1px] w-full border-white pb-3">
+        <h3 className="text-lg font-semibold text-white">{project.title}</h3>
+        <div className="flex gap-2 text-white text-[0.7rem] font-extralight">
+          {project.stack.map((lang: string) => (
+            <p key={lang}>{lang}</p>
+          ))}
         </div>
-        <motion.div
-          className="relative w-full lg:w-85 h-[420px]  rounded-[40px]"
-          whileHover={{ scale: 1.02 }}
-          transition={{ type: "spring", stiffness: 100 }}
-        >
-          <div className="absolute bottom-0 lg:w-[75%] lg:h-[325px] bg-[#31343a79] rounded-t-[40px] left-1/2 -translate-x-1/2 z-[10]" />
-          <div className="absolute bottom-0 lg:w-[85%] lg:h-[315px] bg-[#31343ad0] rounded-t-[40px] left-1/2 -translate-x-1/2 z-[12]" />
+      </div>
+
+      {/* CONTENIDO + IMAGEN */}
+      <motion.div
+        className="relative w-full lg:w-85 h-[420px] rounded-[40px] overflow-visible"
+        whileHover={{ scale: 1.02 }}
+        transition={{ type: "spring", stiffness: 100 }}
+      >
+        <Link to={`/project/${project.id}`} state={project}>
+          <div className="absolute bottom-0 lg:w-[75%] lg:h-[325px] bg-[#31343a79] rounded-t-[40px] left-1/2 -translate-x-1/2 z-10" />
+          <div className="absolute bottom-0 lg:w-[85%] lg:h-[315px] bg-[#31343ad0] rounded-t-[40px] left-1/2 -translate-x-1/2 z-20" />
           <img
             src={project.frontImage}
             alt={project.title}
-            className="absolute bottom-0 left-0 w-full h-[300px] object-cover rounded-[40px] z-[20]"
+            className="absolute bottom-0 left-0 w-full h-[300px] object-cover rounded-[40px] z-[25]"
           />
-          <div className="hidden lg:flex absolute right-[0px] top-[298px] w-[20px] lg:w-[40px] h-[40px] z-30 bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
-          {/*<div className="hidden lg:flex absolute left-[165.5px] lg:left-[166px] bottom-[1px] lg:w-[40px] w-[20px] h-[40px] z-30 bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />*/}
-        </motion.div>
+        </Link>
+
+        {/* CURVAS + BOTÓN EN ORDEN CORRECTO */}
         <div
           id="tab-curve"
-          className="absolute overflow-visible bottom-0 right-0 z-[100] flex items-center justify-center after:bg-[#0F1724] rounded-l-[50px] p-[1rem] after:w-[240px] after:h-[127px] after:content-[''] after:absolute after:top-[-5] after:rounded-tl-[40px] after:rounded-r-none"
-        >
-          <p>
-            <span></span>
-          </p>
-        </div>
+          className="absolute overflow-visible bottom-0 right-0 z-[90] flex items-center justify-center after:bg-[#0F1724] rounded-l-[50px] p-[1rem] after:w-[240px] after:h-[127px] after:content-[''] after:absolute after:top-[-45px] after:rounded-tl-[40px] after:rounded-r-none"
+        />
+        <div
+          id="curve-one"
+          className="absolute bottom-[-12px] right-37 z-[90] flex after:content-[''] w-8 h-10 rounded-br-[50px] after:bg-[#0F1724]"
+        />
         <button
           onClick={() =>
             window.open(project.deploy, "_blank", "noopener,noreferrer")
@@ -51,7 +54,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
           {project.buttonText}
           <GoArrowUpRight className="transition-transform duration-300 ease-in-out group-hover:rotate-45" />
         </button>
-      </Link>
+      </motion.div>
     </div>
   );
 };
