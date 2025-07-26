c2920ab75cf4ab8ca9c87219d17051f0a5618c84
emanuelpps <emanuelpages.ps@gmail.com>
2025-07-25 22:55:37 -0300

    fix

--- src/sections/Projects/components/ProjectCard.tsx
+++ src/sections/Projects/components/ProjectCard.tsx
@@ -32,7 +32,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
             className="absolute bottom-0 left-0 w-full h-[300px] object-cover rounded-[40px] z-[20]"
           />
           <div className="hidden lg:flex absolute right-[0px] top-[298px] w-[20px] lg:w-[40px] h-[40px] z-30 bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
-          <div className="hidden lg:flex absolute left-[165.5px] lg:left-[166px] bottom-[1px] lg:w-[40px] w-[20px] h-[40px] z-30 bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />
+          {/*<div className="hidden lg:flex absolute left-[165.5px] lg:left-[166px] bottom-[1px] lg:w-[40px] w-[20px] h-[40px] z-30 bg-transparent rounded-br-[40px] shadow-[8px_12px_0px_4px_#0F1724]" />*/}
         </motion.div>
         <div
           id="tab-curve"
@@ -52,11 +52,6 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
           <GoArrowUpRight className="transition-transform duration-300 ease-in-out group-hover:rotate-45" />
         </button>
       </Link>
-      <div className="absolute overflow-visible bottom-0 right-0 z-[90] flex items-center justify-center after:bg-[#0F1724] rounded-l-[50px] p-[1rem] after:w-[240px] after:h-[127px] after:content-[''] after:absolute after:top-[-5] after:rounded-tl-[40px] after:rounded-r-none"></div>
-      <div
-        id="curve-one"
-        className="absolute bottom-[-12px] right-37 z-[100] flex after:content-[''] w-8 h-10 rounded-br-[50px] after:bg-[#0F1724]"
-      ></div>
     </div>
   );
 };
