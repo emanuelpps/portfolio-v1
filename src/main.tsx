776d4a6eed7e0bfe70d4004e837668440c01be42
emanuelpps <emanuelpages.ps@gmail.com>
2026-06-12 21:53:29 -0300

    feat: lenis smooth-scroll provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "lenis/dist/lenis.css";
 import { BrowserRouter } from "react-router-dom";
 import { ScrollProvider } from "@/context/ScrollContext";
+import { SmoothScroll } from "@/lib/SmoothScroll";
 import App from "@/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ScrollProvider>
-        <App />
+        <SmoothScroll>
+          <App />
+        </SmoothScroll>
       </ScrollProvider>
     </BrowserRouter>
   </StrictMode>
